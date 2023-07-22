0def545a6fc05f3c7a0dba08f06305533a2a075e
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-07-22 23:12:28 +0300

    Adding assignment api

--- javascript/assignment.js
+++ javascript/assignment.js
@@ -6,7 +6,7 @@ const classesList = document.getElementById("ul-2");
 classArray.forEach((post) => {
     let listClass = document.createElement("li")
     listClass.innerHTML = `
-  <input type="checkbox">
+  <input name="class_room_id" type="checkbox">
   <span>${post}</span>`
         ;
     classesList.appendChild(listClass)
@@ -18,3 +18,32 @@ function displayClasses() {
 }
 
 arrow2.addEventListener('click', displayClasses)
+
+function addAssignment() {
+
+    let assign = document.getElementById("assign")
+    assign.addEventListener('click', async function () {
+        let description = document.getElementById("description").value;
+        let instructions = document.getElementById("instructions").value;
+        let time = document.getElementById("time").value;
+
+        let formData = new FormData();
+        formData.append("title", description)
+        formData.append("instarction", instructions)
+        formData.append("due_date", time)
+
+        let options = {
+            method: "POST",
+            body: formData
+        }
+        try {
+            const response = await fetch("http://localhost/Google-Classroom-Backend/addAssignment.php", options)
+            let json = response.json()
+            console.log(json)
+        }
+        catch (e){
+            console.log("Failed to fetch", e)
+        }
+    })
+
+}
