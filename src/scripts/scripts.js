dc3bc3e54e74215d81415609f42eaa7c722e14ea
unknown <youssefhoussein701@gmail.com>
2023-07-23 14:39:21 +0300

    add the javascript file, fetch the data from the APIs and show it in the page

---
+++ src/scripts/scripts.js
@@ -0,0 +1,61 @@
+const pages = {}
+
+pages.teacher_url  = "http://localhost/Google-Classroom-Backend/teachers.php";
+pages.student_url ="http://localhost/Google-Classroom-Backend/students.php";
+
+pages.createItem = (first_name,last_name) => {
+    return `
+    <div class="user-info flex">
+        <img class="user-image" src="https://lh3.googleusercontent.com/a/default-user=s32-c" alt="profile image">
+        <div>${first_name} ${last_name}</div>
+    </div>
+    `
+}
+const teacher_body = document.getElementById("teacher-body")
+const student_body = document.getElementById("student-body")
+pages.getTeacher = async  () =>{
+  try{
+    const response = await fetch(pages.teacher_url)
+    const json = await response.json()
+    json.forEach(teacher => {
+        const firstName = teacher.first_name;
+        const lastName = teacher.last_name;
+        const new_item = pages.createItem(firstName,lastName)
+        teacher_body.innerHTML += new_item
+        
+    });
+    
+  }
+  catch(e){
+    console.log("Error: "+e)
+  }
+
+}
+pages.getStudent = async  () =>{
+    try{
+      const response = await fetch(pages.student_url)
+      const json = await response.json()
+      json.forEach(student => {
+          const s_firstName = student.first_name;
+          const s_lastName = student.last_name;
+          const new_item = pages.createItem(s_firstName,s_lastName)
+          student_body.innerHTML += new_item
+          console.log(new_item)
+      });
+      
+    }
+    catch(e){
+      console.log("Error: "+e)
+    }
+  
+  }
+  
+pages.page_index = async () => {
+    await pages.getTeacher()
+    
+
+}
+
+pages.loadForm = (page) => {
+    eval("pages.page_" + page + "();")
+}
