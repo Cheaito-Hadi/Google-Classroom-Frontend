1909566a4be99a14fcc812831a350376cc70c001
Mohammad Awada <awada@Mohammads-MacBook-Air.local>
2023-07-23 22:03:28 +0300

    adding svg elements to title box

--- src/js/display.js
+++ src/js/display.js
@@ -1,16 +1,28 @@
 const displayTitles = async () => {
   try {
-    const response = await fetch("http://localhost:8080/Google-Classroom-Backend/stream.php");
+    const response = await fetch("http://localhost:8080/stream.php");
     const data = await response.json();
-
-
+    const assignment_title = data.titles;
   const titlesContainer = document.getElementById("titles-container");
 
-    data.forEach(title => {
+    assignment_title.forEach(title => {
+      console.log(title);
       const divElement = document.createElement("div");
-      divElement.textContent = title;
-      divElement.classList.add("title-box");
+      const svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+      svgElement.setAttribute("focusable", "false");
+      svgElement.setAttribute("width", "24");
+      svgElement.setAttribute("height", "24");
+      svgElement.setAttribute("viewBox", "0 0 24 24");
+      const pathElement = document.createElementNS("http://www.w3.org/2000/svg", "path");
+      pathElement.setAttribute("d", "M18 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 18H6V4h2v8l2.5-1.5L13 12V4h5v16z");
+      svgElement.appendChild(pathElement);
+      divElement.appendChild(svgElement);
+      const titleElement = document.createElement("span");
+      titleElement.textContent = `posted a new assignment: ${title}`;
+      divElement.appendChild(titleElement);
+      divElement.classList.add("title-box")
       titlesContainer.appendChild(divElement);
     });
   } catch(error) {console.log(error)};
 };
+displayTitles();
