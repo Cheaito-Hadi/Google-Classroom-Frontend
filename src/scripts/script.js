3ac8b5385c8b3380ceb54b0c9232f59046c4b807
unknown <youssefhoussein701@gmail.com>
2023-07-24 15:16:25 +0300

    display the fetch result on the page

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,7 +1,12 @@
 const pages = {}
 
 pages.assignment_url ="http://localhost/Google-Classroom-Backend/assignments.php"
-const post_date =0
+
+const teacher_names = document.getElementsByClassName("teacher-name")
+const assignment_title = document.getElementById("assignment-title")
+const assignment_instructions =document.getElementById("instructions")
+const assignment_due_date = document.getElementById("due-date")
+const assignment_post_date = document.getElementById("start-date")
 pages.getAssignmentInfo = async () => {
     try{
         const response = await fetch(pages.assignment_url)
@@ -12,34 +17,22 @@ pages.getAssignmentInfo = async () => {
             const first_name = element.first_name
             const last_name =element.last_name
             const due_date = element.due_date
-            post_date = element.post_date
+            const post_date = element.post_date
+
+            for(let i =0; i<teacher_names.length ;i++){
+                teacher_names[i].innerHTML+=first_name +" "+ last_name
+            }
+            assignment_title.innerHTML = title
+            assignment_instructions.innerHTML = instructions
+            assignment_due_date.innerHTML = "Due "+ due_date
+            assignment_post_date.innerHTML=post_date
         });
     }
     catch(e){
         console.log("Error : "+e)
     }
 }
-console.log(post_date)
 
 
 
 
-
-
-// pages.getStudent = async  () =>{
-//     try{
-//       const response = await fetch(pages.student_url)
-//       const json = await response.json()
-//       json.forEach(student => {
-//           const s_firstName = student.first_name;
-//           const s_lastName = student.last_name;
-//           const new_item = pages.createItem(s_firstName,s_lastName)
-//           student_body.innerHTML += new_item
-//       });
-      
-//     }
-//     catch(e){
-//       console.log("Error: "+e)
-//     }
-  
-//   }
