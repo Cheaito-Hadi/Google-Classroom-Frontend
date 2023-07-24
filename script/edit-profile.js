71211a8b633ce717ec079d48a528ce0721519585
MhdTarhini <mohamedtarhini95@gmail.com>
2023-07-24 21:20:18 +0300

    post updated user info

--- script/edit-profile.js
+++ script/edit-profile.js
@@ -3,7 +3,6 @@ const user = JSON.parse(localStorage.getItem("user"));
 // add user profile pic - navbar and user-info-list
 let profile_pic = document.querySelectorAll(".profile-pic");
 profile_pic.forEach((ele) => {
-  console.log(ele);
   ele.style.backgroundImage = `url(${user.image})`;
 });
 document.querySelector(".user-name").innerHTML = user.name;
@@ -42,31 +41,63 @@ inputFields.forEach((inputField, index) => {
 });
 const update_btn = document.querySelector(".update");
 const upload_image = document.getElementById("profile-image");
-console.log(upload_image);
 
 update_btn.addEventListener("click", async () => {
-  const user = JSON.parse(localStorage.getItem("user"));
-  const file = upload_image.files[0];
+  const update_firstname = document.getElementById("first-name").value;
+  const update_lastname = document.getElementById("last-name").value;
+  const update_email = document.getElementById("email").value;
+  const update_password = document.getElementById("password").value;
+
+  if (upload_image.files[0] !== undefined) {
+    const user = JSON.parse(localStorage.getItem("user"));
+    const file = upload_image.files[0];
+    const user_id = user.id;
+
+    const image_data = new FormData();
+
+    image_data.append("profile_image", file);
+    image_data.append("user_id", user_id);
+
+    try {
+      const response = await fetch(
+        "http://localhost/Google-Classroom-Backend/upload-images.php",
+        {
+          method: "POST",
+          body: image_data,
+        }
+      );
+      const res = await response.json();
+      user.image = `http://localhost/Google-Classroom-Backend/${res.image_path}`;
+      localStorage.setItem("user", JSON.stringify(user));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  const user_data = new FormData();
+  if (update_firstname !== "") {
+    user_data.append("first-name", update_firstname);
+  }
+  if (update_lastname !== "") {
+    user_data.append("last-name", update_lastname);
+  }
+  if (update_email !== "") {
+    user_data.append("email", update_email);
+  }
+  if (update_password !== "") {
+    user_data.append("password", update_password);
+  }
   const user_id = user.id;
-  console.log(file);
-  console.log(user_id);
-  let data = new FormData();
-  data.append("profile_image", file);
-  data.append("user_id", user_id);
+  user_data.append("user_id", user_id);
 
   try {
-    const response = await fetch(
-      "http://localhost/Google-Classroom-Backend/upload-images.php",
+    const user_response = await fetch(
+      "http://localhost/Google-Classroom-Backend/update-profile.php",
       {
         method: "POST",
-        body: data,
+        body: user_data,
       }
     );
-    console.log(response);
-    const res = await response.json();
-    console.log(res.image_path);
-    user.image = `http://localhost/Google-Classroom-Backend/${res.image_path}`;
-    localStorage.setItem("user", JSON.stringify(user));
+    const user_res = await user_response.json();
   } catch (error) {
     console.error(error);
   }
