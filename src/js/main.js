9b24d0a05575221d0a2f11a966f1ee3272e47dd2
Mohammad Awada <awada@Mohammads-MacBook-Air.local>
2023-07-24 20:50:05 +0300

    creating main.js to call functions

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+const displayAllTitles = async () => {
+  try {
+    await displayTitles();
+    await display_announcements();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", displayAllTitles);
+
+const postButton = document.getElementById("announce"); 
+postButton.addEventListener("click", displayAllTitles);
